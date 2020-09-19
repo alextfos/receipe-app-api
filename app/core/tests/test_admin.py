00bf57b5db1afa98cd247c430c4442c2621e07ab
Alejandro Traver <alextfos@gmail.com>
2020-09-19 19:46:58 +0200

    Chapter 33

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -25,3 +25,11 @@ class AdminTestCase(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_page_changes(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # /admin/core/user/
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
