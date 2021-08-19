607b21128b2073e97c6f366ec081d951c947d256
Alejandro Traver <alextfos@gmail.com>
2021-08-19 23:48:23 +0200

    Chapter 69

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -38,3 +38,4 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeDetailSerializer(RecipeSerializer):
     """Serialize a recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
